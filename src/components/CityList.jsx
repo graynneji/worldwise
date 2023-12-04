448b5118f2c125a4530f527356a288b1f3bee0a6
Graay Ukaegbu <graynneji405@gmail.com>
2023-12-04 03:24:28 -0800

    error in deployment - corrected CityItem

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import Spinner from "./Spinner";
 import styles from "./CityList.module.css";
-import CityItem from "./cityItem";
+import CityItem from "./CityItem";
 // import CityItem from "./CityItem";
 import Message from "./Message";
 import { useCities } from "../contexts/citiesContext";
